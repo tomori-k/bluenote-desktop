f529afe842a724c73c7b8fc093806178001fcb0b
tomori-k <108829509+tomori-k@users.noreply.github.com>
2023-12-18 01:56:29 +0900

    ヘッダとなっかみの divider

--- src/renderer-react/src/components/Closable.tsx
+++ src/renderer-react/src/components/Closable.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function Closable({ header, children, onClose }: Props) {
   return (
-    <div className="bg-midnight-800 border-l-midnight-500 grid grid-rows-[auto_minmax(0,_1fr)] border-l">
+    <div className="bg-midnight-800 border-l-midnight-600 divide-midnight-600 grid grid-rows-[auto_minmax(0,_1fr)] divide-y border-l">
       <div className="flex h-11 items-center justify-between">
         {header}
         <button className="px-5" type="button" onClick={onClose}>
