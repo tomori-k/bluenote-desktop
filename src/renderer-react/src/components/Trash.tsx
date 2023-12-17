945f84c0ab5948b32e2bc5570219fb5b7a5e7d3d
tomori-k <108829509+tomori-k@users.noreply.github.com>
2023-12-17 16:31:36 +0900

    ごみ箱機能実装

--- src/renderer-react/src/components/Trash.tsx
+++ src/renderer-react/src/components/Trash.tsx
@@ -1,17 +1,94 @@
-import { useEffect, useRef } from 'react'
-import { useNotePagination } from './ThreadView'
+import { useEffect, useRef, useState } from 'react'
+import { ContextMenu } from './SideMenu'
+import { Note } from '@prisma/client'
 
-export default function Trash() {
-  const refLoading = useRef<HTMLLIElement>(null)
-  const refOnReachedLast = useRef(() => {})
-  const { notes, hasLoadedAll, hasErrorOccured, loadMore } = useNotePagination(
-    async (lastId, count) => {
-      return await window.api.findNotesInTrash('', lastId, count)
+function useTrashNoteList(
+  loadNext: (lastId: string | null, count: number) => Promise<Note[]>,
+  restoreNote: (note: Note) => Promise<void>,
+  deleteNote: (note: Note) => Promise<void>
+) {
+  const [hasErrorOccured, setHasErrorOccured] = useState(false)
+  const [hasLoadedAll, setHasLoadedAll] = useState(false)
+  const [notes, setNotes] = useState<Note[]>([])
+
+  async function onNoteRestoreClicked(note: Note) {
+    try {
+      await restoreNote(note)
+      setNotes(notes.filter((n) => n.id !== note.id))
+    } catch (e) {
+      setHasErrorOccured(true)
     }
-  )
+  }
+
+  async function onNoteDeleteClicked(note: Note) {
+    try {
+      await deleteNote(note)
+      setNotes(notes.filter((n) => n.id !== note.id))
+    } catch (e) {
+      setHasErrorOccured(true)
+    }
+  }
 
   async function onReachedLast() {
-    await loadMore()
+    const count = 100 // ページ単位
+
+    try {
+      const lastId = notes.length > 0 ? notes[notes.length - 1].id : null
+      const items = await loadNext(lastId, count)
+
+      setNotes([...notes, ...items])
+      if (items.length < count) setHasLoadedAll(true)
+    } catch (e) {
+      setHasErrorOccured(true)
+    }
+  }
+
+  return {
+    notes,
+    hasLoadedAll,
+    hasErrorOccured,
+    onReachedLast,
+    onNoteRestoreClicked,
+    onNoteDeleteClicked,
+  }
+}
+
+export default function Trash() {
+  const {
+    notes,
+    hasLoadedAll,
+    hasErrorOccured,
+    onReachedLast,
+    onNoteRestoreClicked,
+    onNoteDeleteClicked,
+  } = useTrashNoteList(
+    (lastId, count) => window.api.findNotesInTrash('', lastId, count),
+    (note) => window.api.restoreNote(note),
+    (note) => window.api.deleteNote(note)
+  )
+  const [contextMenuState, setContextMenuState] = useState<{
+    left: number
+    top: number
+    note: Note
+  } | null>(null)
+
+  const refLoading = useRef<HTMLLIElement>(null)
+  const refOnReachedLast = useRef(() => {})
+
+  function onNoteMenuClicked(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    note: Note
+  ) {
+    e.stopPropagation()
+
+    const bounds = e.currentTarget.getBoundingClientRect()
+
+    // TODO: right で指定したい
+    setContextMenuState({
+      left: bounds.left - 100,
+      top: bounds.top,
+      note,
+    })
   }
 
   refOnReachedLast.current = onReachedLast
@@ -43,13 +120,36 @@ export default function Trash() {
 
       <ul className="overflow-y-auto">
         {notes.map((note) => (
-          <li key={note.id}>
+          <li className="group relative" key={note.id}>
             <p>{note.createdAt.toUTCString()}</p>
             <p>{note.content}</p>
+            <button
+              type="button"
+              className="collapse absolute right-0 top-0 group-hover:visible"
+              onClick={(e) => onNoteMenuClicked(e, note)}
+            >
+              ...
+            </button>
           </li>
         ))}
         {!hasLoadedAll && <li ref={refLoading}>Loading</li>}
       </ul>
+
+      {contextMenuState && (
+        <ContextMenu
+          position={contextMenuState}
+          onClose={() => setContextMenuState(null)}
+        >
+          <ul>
+            <li onClick={() => onNoteRestoreClicked(contextMenuState.note)}>
+              もとに戻す
+            </li>
+            <li onClick={() => onNoteDeleteClicked(contextMenuState.note)}>
+              完全に削除
+            </li>
+          </ul>
+        </ContextMenu>
+      )}
     </div>
   )
 }
